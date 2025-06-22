cd5ab4f14df23d3f255afc92278cb1830aabce51
egg528 <rnjsdntjr26@daum.net>
2025-06-22 17:11:02 +0900

    동일한 주문을 어떻게 정의할 것인가?

--- payment-service/src/main/kotlin/com/example/paymentservice/adapter/in/web/request/CheckoutRequest.kt
+++ payment-service/src/main/kotlin/com/example/paymentservice/adapter/in/web/request/CheckoutRequest.kt
@@ -6,5 +6,8 @@ data class CheckoutRequest (
   val cartId: Long = 1,
   val productIds: List<Long> = listOf(1, 2, 3),
   val buyerId: Long = 1,
+  // 해당 주문을 식별할 수 있는 무언가가 필요함
+  // 단순히 LocalDateTime.now().toString()로는 안 됨
+  // 정책 결정이 필요해 보인다.
   val seed: String = LocalDateTime.now().toString()
 )
